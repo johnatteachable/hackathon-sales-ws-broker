c4ba1a3fb1aebfe18f6980736ef1de9a280e4710
John T Scherer <john.scherer@teachable.com>
2022-12-01 11:11:05 -0500

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+type RecentSaleData struct {
+}
+
+// Creat API to consume RecentSaleData
+// Establish connection to websocket and forward data from API request
+func main() {
+	fmt.Println("Starting sales websocket broker")
+
+}
